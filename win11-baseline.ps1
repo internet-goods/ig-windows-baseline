a35ce14f6ba3ec57aa531850eaf5beb34dd62968
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-03-02 20:06:34 -0600

    Create win11-baseline.ps1

---
+++ win11-baseline.ps1
@@ -0,0 +1,14 @@
+#app baseline
+Invoke-WebRequest https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/scc-5.10.1_Windows_bundle.zip
+Invoke-WebRequest https://download.sysinternals.com/files/ProcessExplorer.zip
+Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.48.1.windows.1/Git-2.48.1-64-bit.exe
+Invoke-WebRequest https://www.clamav.net/downloads#collapseWindowslts =3600&X-Amz-SignedHeaders=host&X-Amz-Signature=41a2edc5834fc0abb0a269cc8f92121777a989b497213d75e579e4544e04a190?https://clamav-site.s3.amazonaws.com/production/release_files/files/000/001/821/original/clamav-1.4.2.win.arm64.msi?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAU7AK5ITMD2IZLINE%2F20250303%2Fus-east-1%2Fs3%2Faws4_request&X
+#CLAMAV
+Invoke-WebRequest https://clamav-site.s3.amazonaws.com/production/release_files/files/000/001/821/original/clamav-1.4.2.win.arm64.msi
+#yara
+Invoke-WebRequest https://objects.githubusercontent.com/github-production-release-asset-2e65be/7037996/8793b738-3bc5-4dd8-91ef-4fdd1dac418e?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250303%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250303T015750Z&X-Amz-Expires=300&X-Amz-Signature=753c038a0702cd300b89f3a761982e958abed79fa26e0733549ca3d71a423f6a&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dyara-v4.5.2-2326-win64.zip&response-content-type=application%2Foctet-stream
+Invoke-WebRequest https://packages.wazuh.com/4.x/windows/wazuh-agent-4.11.0-1.msi
+https://dl1.cdn.filezilla-project.org/client/FileZilla_3.68.1_x86_64-linux-gnu.tar.xz?h=zuW-H_2uBTephsSUVampWQ&x=1740971160
+#git
+git clome https://github.com/Sneakysecdoggo/Wynis
+git clone https://github.com/scipag/HardeningKitty
