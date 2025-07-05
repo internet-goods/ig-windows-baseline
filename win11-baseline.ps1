540fffa88d053d8a02ecda2ae1080c734454ab26
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-07-05 16:11:25 -0500

    Update win11-baseline.ps1

--- win11-baseline.ps1
+++ win11-baseline.ps1
@@ -10,7 +10,8 @@ sc upnphost start= disabled
 #modules
 Install-Module -Name SpeculationControl
 Import-Module -Name SpeculationControl
-
+#dism
+dism /online /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64
 #auditpol
 auditpol.exe /get /category:* > auditpol_beforehardening.txt
 #dostuff
