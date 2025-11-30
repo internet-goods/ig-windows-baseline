51030183edcf085aeaf58ed1596ed984c1d9cbb5
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-11-30 08:12:03 -0600

    Update and rename win.bat to winget.bat

--- winget.bat
+++ winget.bat
@@ -1,16 +1,14 @@
 #IG Windows Baseline
 #Install Apps
-mkdir ig
-cd ig
 #winget
 curl https://aka.ms/getwinget > winget.msixbundle
-#Add-AppxPackage winget.msixbundle
+Add-AppxPackage winget.msixbundle
 #install git
-#winget install --id Git.Git -e --source winget
+winget install --id Git.Git -e --source winget
 #hardening
 
 
-curl https://github.com/niwc-atlantic/scap-scc/blob/main/release_archive/SCC_5.8/scc-5.8_Windows_bundle.zip > scc-5.8_Windows_bundle.zip
+#curl https://github.com/niwc-atlantic/scap-scc/blob/main/release_archive/SCC_5.8/scc-5.8_Windows_bundle.zip > scc-5.8_Windows_bundle.zip
 
 #monitoring
 winget install --id=osquery.osquery -e
@@ -22,7 +20,7 @@ winget install -e --id Python.Python.3.13 --scope machine
 winget install --id=StrawberryPerl.StrawberryPerl -e
 winget install RubyInstallerTeam.Ruby.3.2
 #ham
-curl https://github.com/gqrx-sdr/gqrx/releases/download/v2.17.7/Gqrx-2.17.7-Windows.zip > Gqrx-2.17.7-Windows.zip
+#curl https://github.com/gqrx-sdr/gqrx/releases/download/v2.17.7/Gqrx-2.17.7-Windows.zip > Gqrx-2.17.7-Windows.zip
 #apps
 winget install -e --id GNU.Wget2
 winget install -e --id Rufus.Rufus
@@ -45,17 +43,17 @@ winget install -e --id Microsoft.Sysinternals.Coreinfo
 #test localsystem
 #https://github.com/redcanaryco/atomic-red-team/
 #test domain
-curl https://github.com/SpecterOps/SharpHound/releases/download/v2.8.0/SharpHound_v2.8.0_windows_x86.zip > SharpHound_v2.8.0_windows_x86.zip
+#curl https://github.com/SpecterOps/SharpHound/releases/download/v2.8.0/SharpHound_v2.8.0_windows_x86.zip > SharpHound_v2.8.0_windows_x86.zip
 #unzip sharphound
-SharpHound.exe -c All
-curl https://github.com/netwrix/pingcastle/releases/download/3.4.2.66/PingCastle_3.4.2.66.zip > PingCastle_3.4.2.66.zip
+#SharpHound.exe -c All
+#curl https://github.com/netwrix/pingcastle/releases/download/3.4.2.66/PingCastle_3.4.2.66.zip > PingCastle_3.4.2.66.zip
 #unzip pingcastle
-PingCastle.exe --healthcheck
+#PingCastle.exe --healthcheck
 
-git clone https://github.com/Sneakysecdoggo/Wynis
-Wynis\wynis.exe audit
-git clone https://github.com/scipag/HardeningKitty
-HardeningKitty\HardeningKitty.ps1 -Mode Analysis -Log
+#git clone https://github.com/Sneakysecdoggo/Wynis
+#Wynis\wynis.exe audit
+#git clone https://github.com/scipag/HardeningKitty
+#HardeningKitty\HardeningKitty.ps1 -Mode Analysis -Log
 
 #IG Baselines
 
