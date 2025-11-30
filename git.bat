dcb7377a57d495c69f987e5bbd0f6d84e9f44af4
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-11-30 08:20:53 -0600

    Add SigmaHQ repository clone command

--- git.bat
+++ git.bat
@@ -1,3 +1,4 @@
 git clone https://github.com/Sneakysecdoggo/Wynis
 git clone https://github.com/scipag/HardeningKitty
 git clone https://github.com/redcanaryco/atomic-red-team
+git clone https://github.com/SigmaHQ/sigma
