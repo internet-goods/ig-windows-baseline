e06c47bb7d54b3b0aff8977c1d3c7006a8d8d10e
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-11-30 09:39:54 -0600

    Change git clone to use --depth 1 for efficiency

---
+++ git-baseline.bat
@@ -0,0 +1,4 @@
+git clone --depth 1 https://github.com/Sneakysecdoggo/Wynis
+git clone --depth 1 https://github.com/scipag/HardeningKitty
+git clone --depth 1 https://github.com/redcanaryco/atomic-red-team
+git clone --depth 1 https://github.com/SigmaHQ/sigma
--- git.bat
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/Sneakysecdoggo/Wynis
-git clone https://github.com/scipag/HardeningKitty
-git clone https://github.com/redcanaryco/atomic-red-team
-git clone https://github.com/SigmaHQ/sigma
