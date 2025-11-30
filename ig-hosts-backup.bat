affb1920a8f94b6f0dc834a526863fcf1c324abb
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-11-30 09:03:02 -0600

    Update ig-hosts-backup.bat

--- ig-hosts-backup.bat
+++ ig-hosts-backup.bat
@@ -1,13 +1,14 @@
 @echo off
 ECHO Baseline Hosts file looking for malicious insertions
 echo based on ig-schdtasks-backup.bat
+set "BackupDir=C:\Windows\Temp\ig-windows-baseline\hosts"
 :: Get the current date in YYYY-MM-DD format
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
     set "CurrentDate=%%c-%%a-%%b"
 )
 :: 1. Check if the root backup directory exists, if not, create it
-IF NOT EXIST "%BackupRoot%" (
-    ECHO Creating backup root directory: %BackupRoot%
-    mkdir "%BackupRoot%"
+IF NOT EXIST "%BackupDir%" (
+    ECHO Creating backup root directory: %BackupDir%
+    mkdir "%BackupDir%"
 )
-copy C:\Windows\Temp\ig-windows-baseline\hosts C:\Windows\Temp\ig-windows-baseline\hosts\%COMPUTERNAME%-hosts-%CurrentDate%.txt /Y
+copy C:\Windows\Temp\ig-windows-baseline\hosts %BackupDir%\%COMPUTERNAME%-hosts-%CurrentDate%.txt /Y
