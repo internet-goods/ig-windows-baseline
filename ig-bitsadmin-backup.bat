ed0300d76cbdfa674ca4b13cb604b1681068d4b9
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-01 07:50:49 -0600

    Create ig-bitsadmin-backup.bat for backup logging
    This batch script creates a backup directory for bitsadmin logs and saves the output of the bitsadmin command to a date-specific text file.

---
+++ ig-bitsadmin-backup.bat
@@ -0,0 +1,16 @@
+setlocal
+:: Get the current date in YYYY-MM-DD format
+for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
+    set "CurrentDate=%%c-%%a-%%b"
+)
+:: Define the base backup directory (e.g., C:\TaskBackups)
+set "BackupDir=C:\Windows\Temp\ig-windows-baseline\bitsadmin"
+:: Create the date-specific directory
+set "TargetDir=%BackupDir%"
+if not exist "%TargetDir%" (
+    mkdir "%TargetDir%"
+    echo Created directory: %TargetDir%
+)
+set "OutputFilePathTXT=%TargetDir%\%COMPUTERNAME%-bitsadmin-%CurrentDate%.txt"
+bitsadmin /list /allusers > "%OutputFilePathTXT%"
+endlocal
