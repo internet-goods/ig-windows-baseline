1e8ae94b1d335bdcaf11415ba6a2e0a2a285c5ba
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-03 17:46:10 -0600

    Update ig-wevtutil-backup.bat

--- ig-wevtutil-backup.bat
+++ ig-wevtutil-backup.bat
@@ -2,7 +2,7 @@ set "BackupDir=C:\Windows\Temp\ig-windows-baseline\wevtutil"
 if not exist "%BackupDir%" (
     mkdir "%BackupDir%"
 )
-set SECURITYEVTX     = %BackupDir%\%COMPUTERNAME%-Security-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+set SECURITYEVTX     = 
 set APPLICATIONEVTX  = %BackupDir%\%COMPUTERNAME%-Application-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set SYSTEMEVTX       = %BackupDir%\%COMPUTERNAME%-System-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set SYSMONEVTX       = %BackupDir%\%COMPUTERNAME%-Sysmon-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
@@ -10,7 +10,7 @@ set POWERSHELLEVTX   = %BackupDir%\%COMPUTERNAME%-PowerShell-%date:~4,2%%date:~7
 set POWERSHELLOPEVTX = %BackupDir%\%COMPUTERNAME%-PowerShellOp-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set DIRECTORYSVCEVTX = %BackupDir%\%COMPUTERNAME%-DirectoryService-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set DFSREPLICATION   = %BackupDir%\%COMPUTERNAME%-DFSReplication-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-wevtutil epl Security %SECURITYEVTX%
+wevtutil epl Security %BackupDir%\%COMPUTERNAME%-Security-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 wevtutil epl Application %APPLICATIONEVTX%
 wevtutil epl System %SYSTEMEVTX%
 wevtutil epl "Microsoft-Windows-Sysmon/Operational" %SYSMONEVTX%
@@ -18,11 +18,3 @@ wevtutil epl "Windows PowerShell" %POWERSHELLEVTX%
 wevtutil epl "Microsoft-Windows-PowerShell/Operational" %POWERSHELLOPEVTX%
 wevtutil epl "Directory Service" %DIRECTORYSVCEVTX%
 wevtutil epl "DFS Replication" %DFSREPLICATION%
-tar -czvf %SECURITYEVTX%.tar.gz --options gzip:compression-level=9 %SECURITYEVTX%
-tar -czvf %APPLICATIONEVTX%.tar.gz --options gzip:compression-level=9 %APPLICATIONEVTX%
-tar -czvf %SYSTEMEVTX%.tar.gz --options gzip:compression-level=9 %SYSTEMEVTX%
-tar -czvf %SYSMONEVTX%.tar.gz --options gzip:compression-level=9 %SYSMONEVTX%
-tar -czvf %POWERSHELLEVTX% --options gzip:compression-level=9 %POWERSHELLEVTX%
-tar -czvf %POWERSHELLOPEVTX%.tar.gz --options gzip:compression-level=9 %POWERSHELLOPEVTX%
-tar -czvf %DIRECTORYSVCEVTX%.tar.gz --options gzip:compression-level=9 %DIRECTORYSVCEVTX%
-tar -czvf %DFSREPLICATION%.tar.gz --options gzip:compression-level=9 %DFSREPLICATION%
