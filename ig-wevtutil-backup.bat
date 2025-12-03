98e5d9aefe55e0c9938a4506be161245e673fe79
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-03 17:51:25 -0600

    Fix SECURITYEVTX assignment in backup script

--- ig-wevtutil-backup.bat
+++ ig-wevtutil-backup.bat
@@ -2,7 +2,7 @@ set "BackupDir=C:\Windows\Temp\ig-windows-baseline\wevtutil"
 if not exist "%BackupDir%" (
     mkdir "%BackupDir%"
 )
-REM set SECURITYEVTX     = 
+set SECURITYEVTX     = %BackupDir%\%COMPUTERNAME%-Security-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set APPLICATIONEVTX  = %BackupDir%\%COMPUTERNAME%-Application-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set SYSTEMEVTX       = %BackupDir%\%COMPUTERNAME%-System-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set SYSMONEVTX       = %BackupDir%\%COMPUTERNAME%-Sysmon-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
@@ -10,7 +10,7 @@ set POWERSHELLEVTX   = %BackupDir%\%COMPUTERNAME%-PowerShell-%date:~4,2%%date:~7
 set POWERSHELLOPEVTX = %BackupDir%\%COMPUTERNAME%-PowerShellOp-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set DIRECTORYSVCEVTX = %BackupDir%\%COMPUTERNAME%-DirectoryService-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
 set DFSREPLICATION   = %BackupDir%\%COMPUTERNAME%-DFSReplication-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-wevtutil epl Security %BackupDir%\%COMPUTERNAME%-Security-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl Security = %SECURITYEVTX%
 wevtutil epl Application %APPLICATIONEVTX%
 wevtutil epl System %SYSTEMEVTX%
 wevtutil epl "Microsoft-Windows-Sysmon/Operational" %SYSMONEVTX%
