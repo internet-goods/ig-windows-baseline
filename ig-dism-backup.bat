7d281a049374c8f005f7a6b00678fb4f46637a43
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-04 10:12:01 -0600

    Update ig-dism-backup.bat

--- ig-dism-backup.bat
+++ ig-dism-backup.bat
@@ -4,3 +4,6 @@ if not exist "%BackupDir%" (
     echo Created directory: %BackupDir%
 )
 dism /Online /Get-Features /Format:Table > %TargetDir%\%COMPUTERNAME%-dismfeatures-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.txt
+REM DEFAULT
+copy c:\Windows\Logs\DISM\dism.log %TargetDir%\%COMPUTERNAME%-dism-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.log
+findstr /c:%date:~10,4%-%date:~4,2%-%date:~7,2% c:\Windows\Logs\DISM\dism.log > %TargetDir%\%COMPUTERNAME%-dismtoday-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.log
