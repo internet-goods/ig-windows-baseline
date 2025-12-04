5f67ba66935a1a99facb68e2ffa8f466c91cb7d3
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-04 07:07:29 -0600

    Update ig-auditpol-backup.bat

--- ig-auditpol-backup.bat
+++ ig-auditpol-backup.bat
@@ -1,16 +1,6 @@
-setlocal
-:: Get the current date in YYYY-MM-DD format
-for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
-    set "CurrentDate=%%c-%%a-%%b"
-)
-:: Define the base backup directory (e.g., C:\TaskBackups)
 set "BackupDir=C:\Windows\Temp\ig-windows-baseline\auditpol"
-:: Create the date-specific directory
-set "TargetDir=%BackupDir%"
-if not exist "%TargetDir%" (
-    mkdir "%TargetDir%"
-    echo Created directory: %TargetDir%
+if not exist "%BackupDir%" (
+    mkdir "%BackupDir%"
+    echo Created directory: %BackupDir%
 )
-set "OutputFilePathTXT=%TargetDir%\%COMPUTERNAME%-auditpol-%CurrentDate%.txt"
-auditpol /list /subcategory:* /r > "%OutputFilePathTXT%"
-endlocal
+auditpol /list /subcategory:* /r > %TargetDir%\%COMPUTERNAME%-auditpol-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.txt
