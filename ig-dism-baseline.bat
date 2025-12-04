f6b2c77417dfcaa7f0f18e3434ae3859f10a9140
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-04 08:16:33 -0600

    Create ig-dism-baseline.bat

---
+++ ig-dism-baseline.bat
@@ -0,0 +1,6 @@
+dism /online /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64
+dism /online /Disable-Feature /FeatureName:TelnetClient
+dism /online /Disable-Feature /FeatureName:TFTP
+dism /online /Disable-Feature /FeatureName:SMB1Protocol
+dism /online /Disable-Feature /FeatureName:MicrosoftWindowsPowershellV2Root
+dism /online /Disable-Feature /FeatureName:SNMP
