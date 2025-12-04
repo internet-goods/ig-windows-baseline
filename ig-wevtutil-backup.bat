7c99ee3c14651359a7bd83691cf29699f0f6b30f
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-03 18:07:35 -0600

    Update ig-wevtutil-backup.bat

--- ig-wevtutil-backup.bat
+++ ig-wevtutil-backup.bat
@@ -2,19 +2,11 @@ set "BackupDir=C:\Windows\Temp\ig-windows-baseline\wevtutil"
 if not exist "%BackupDir%" (
     mkdir "%BackupDir%"
 )
-set SECURITYEVTX     = %BackupDir%\%COMPUTERNAME%-Security-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-set APPLICATIONEVTX  = %BackupDir%\%COMPUTERNAME%-Application-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-set SYSTEMEVTX       = %BackupDir%\%COMPUTERNAME%-System-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-set SYSMONEVTX       = %BackupDir%\%COMPUTERNAME%-Sysmon-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-set POWERSHELLEVTX   = %BackupDir%\%COMPUTERNAME%-PowerShell-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-set POWERSHELLOPEVTX = %BackupDir%\%COMPUTERNAME%-PowerShellOp-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-set DIRECTORYSVCEVTX = %BackupDir%\%COMPUTERNAME%-DirectoryService-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-set DFSREPLICATION   = %BackupDir%\%COMPUTERNAME%-DFSReplication-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
-wevtutil epl Security = %SECURITYEVTX%
-wevtutil epl Application %APPLICATIONEVTX%
-wevtutil epl System %SYSTEMEVTX%
-wevtutil epl "Microsoft-Windows-Sysmon/Operational" %SYSMONEVTX%
-wevtutil epl "Windows PowerShell" %POWERSHELLEVTX%
-wevtutil epl "Microsoft-Windows-PowerShell/Operational" %POWERSHELLOPEVTX%
-wevtutil epl "Directory Service" %DIRECTORYSVCEVTX%
-wevtutil epl "DFS Replication" %DFSREPLICATION%
+wevtutil epl Security  %BackupDir%\%COMPUTERNAME%-Security-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl Application %BackupDir%\%COMPUTERNAME%-Application-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl System %BackupDir%\%COMPUTERNAME%-System-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl "Microsoft-Windows-Sysmon/Operational" %BackupDir%\%COMPUTERNAME%-Sysmon-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl "Windows PowerShell" %BackupDir%\%COMPUTERNAME%-PowerShell-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl "Microsoft-Windows-PowerShell/Operational" %BackupDir%\%COMPUTERNAME%-PowerShellOp-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl "Directory Service" %BackupDir%\%COMPUTERNAME%-DirectoryService-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
+wevtutil epl "DFS Replication" %BackupDir%\%COMPUTERNAME%-DFSReplication-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~0,2%%TIME:~3,2%.evtx
