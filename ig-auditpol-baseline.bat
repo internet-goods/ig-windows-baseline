8401e9ecde978a3d988ea5b07b0a9e6f6e649240
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-04 06:59:56 -0600

    Create ig-auditpol-baseline.bat

---
+++ ig-auditpol-baseline.bat
@@ -0,0 +1,23 @@
+auditpol /set /subcategory:"Credential Validation" /success:enable /failure:enable
+auditpol /set /subcategory:"Security Group Management" /success:enable /failure:enable
+auditpol /set /subcategory:"User Account Management" /success:enable /failure:enable
+auditpol /set /subcategory:"Process Creation" /success:enable /failure:enable
+auditpol /set /subcategory:"Account Lockout" /success:enable
+auditpol /set /subcategory:"Logoff" /success:enable
+auditpol /set /subcategory:"Logon" /success:enable /failure:enable
+auditpol /set /subcategory:"Special Logon" /success:enable /failure:enable
+auditpol /set /subcategory:"File Share" /success:enable /failure:enable
+auditpol /set /subcategory:"Account Lockout" /success:enable
+auditpol /set /subcategory:"Audit Policy Change" /success:enable /failure:enable
+auditpol /set /subcategory:"Authentication Policy Change" /success:enable /failure:enable
+auditpol /set /subcategory:"Authorization Policy Change" /success:enable /failure:enable
+auditpol /set /subcategory:'Sensitive Privilege Use' /success:enable /failure:enable
+auditpol /set /subcategory:"IPsec Driver" /failure:enable
+auditpol /set /subcategory:"Other System Events" /success:enable /failure:enable
+auditpol /set /subcategory:"Security State Change" /success:enable /failure:enable
+auditpol /set /subcategory:"Security System Extension" /success:enable /failure:enable
+auditpol /set /subcategory:"System Integrity" /success:enable /failure:enable
+auditpol /set /subcategory:"Other Policy Change Events" /success:enable /failure:enable
+auditpol /set /subcategory:"Other Logon/Logoff Events" /success:enable /failure:enable
+auditpol /set /subcategory:"Detailed File Share" /failure:enable
+auditpol /set /subcategory:"MPSSVC Rule-Level Policy Change" /success:enable /failure:enable
