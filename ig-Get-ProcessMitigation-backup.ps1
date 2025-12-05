169d4db83016cf5201091da3b7c23a0635f2b359
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-05 08:26:28 -0600

    Update ig-Get-ProcessMitigation-backup.ps1

--- ig-Get-ProcessMitigation-backup.ps1
+++ ig-Get-ProcessMitigation-backup.ps1
@@ -1,3 +1,3 @@
 mkdir Get-ProcessMitigation
 Get-ProcessMitigation -System > Get-ProcessMitigation/Get-ProcessMitigation-System-$(Get-Date -Format 'yyy-mm-dd-HH-MM').txt
-Get-ProcessMitigation -System > Get-ProcessMitigation/Get-ProcessMitigation-$(Get-Date -Format 'yyy-mm-dd-HH-MM').txt
+Get-ProcessMitigation > Get-ProcessMitigation/Get-ProcessMitigation-$(Get-Date -Format 'yyy-mm-dd-HH-MM').txt
