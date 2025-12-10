c6953f1451540adaad427da9ff79959af3fb1327
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-09 20:16:03 -0600

    Update ig-wevtutil-backup.bat

--- ig-wevtutil-backup.bat
+++ ig-wevtutil-backup.bat
@@ -1,4 +1,5 @@
 set "BackupDir=C:\Windows\Temp\ig-windows-baseline\wevtutil\%date:~4,2%%date:~7,2%%date:~10,4%"
+echo Extract the Days logs into %BackupDir%
 if not exist "%BackupDir%" (
     mkdir "%BackupDir%"
 )
@@ -21,3 +22,7 @@ wevtutil epl "Microsoft-Windows-PowerShell/Operational" %BackupDir%\%COMPUTERNAM
 wevtutil epl "Directory Service" %BackupDir%\%COMPUTERNAME%-DirectoryService-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.evtx
 wevtutil epl "DFS Replication" %BackupDir%\%COMPUTERNAME%-DFSReplication-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.evtx
 )
+REM Install https://www.7-zip.org/a/7z2501.exe  7zip 7z2301-x64.exe /S
+ECHO ZIP the days logs into C:\Windows\Temp\ig-windows-baseline\wevtutil\
+"C:\Program Files\7-zip\7z.exe" a -t7z -m0=lzma2 -mx=9 "C:\Windows\Temp\ig-windows-baseline\wevtutil\%COMPUTERNAME%-wevtutil-%date:~4,2%%date:~7,2%%date:~10,4%.7z" "%BackupDir%\*"
+REM CLEANUP del %BackupDir%\*
