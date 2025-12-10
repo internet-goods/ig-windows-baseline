4374bd33d520090fde2508e94aaf65f868f45017
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-09 21:54:51 -0600

    Update ig-auditpol-backup.bat

--- ig-auditpol-backup.bat
+++ ig-auditpol-backup.bat
@@ -3,4 +3,5 @@ if not exist "%BackupDir%" (
     mkdir "%BackupDir%"
     echo Created directory: %BackupDir%
 )
-auditpol /list /subcategory:* /r > %BackupDir%\%COMPUTERNAME%-auditpol-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.txt
+REM auditpol /list /subcategory:* /r > %BackupDir%\%COMPUTERNAME%-auditpol-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.txt
+auditpol /get /category:* > %BackupDir%\%COMPUTERNAME%-auditpol-%date:~4,2%%date:~7,2%%date:~10,4%-%TIME:~3,2%%TIME:~6,2%.txt
