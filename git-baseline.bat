9b7093a4180375727973e15be324e65fae5a2d05
internet-goods <4577617+internet-goods@users.noreply.github.com>
2025-12-14 06:36:06 -0600

    Update and rename git-baseline.bat to ig-git-baseline.bat

--- git-baseline.bat
+++
@@ -1,4 +0,0 @@
-git clone --depth 1 https://github.com/Sneakysecdoggo/Wynis
-git clone --depth 1 https://github.com/scipag/HardeningKitty
-git clone --depth 1 https://github.com/redcanaryco/atomic-red-team
-git clone --depth 1 https://github.com/SigmaHQ/sigma
---
+++ ig-git-baseline.bat
@@ -0,0 +1,30 @@
+echo Internet-Goods's Favorite git repos...
+git clone --depth 1 https://github.com/Sneakysecdoggo/Wynis
+git clone --depth 1 https://github.com/scipag/HardeningKitty
+git clone --depth 1 https://github.com/redcanaryco/atomic-red-team
+git clone --depth 1 https://github.com/SigmaHQ/sigma
+echo Inspiration from the rest...
+echo git clone https://github.com/simeononsecurity/Windows-Optimize-Harden-Debloat
+echo git clone https://github.com/mitre/google-chrome-v2r6-stig-baseline
+echo git clone https://github.com/milgradesec/windows-settings
+echo #https://github.com/fishilico/generic-config/blob/2593f3f7d5f0a891e278d773c0cd3b2120b656f0/windows/hardening_script.bat#L113
+echo git clone https://github.com/fishilico/generic-config
+echo git clone https://github.com/ZephrFish/WindowsHardeningScript
+echo git clone https://github.com/jkerai1/WindowsHardeningScripts
+echo git clone https://github.com/dend/windows-dev-box/
+echo git clone https://github.com/michalzobec/PS-STIG-Scanner
+echo git clone https://github.com/blue101010/WindowsDebloater
+echo git clone https://github.com/azurejoga/Aurora-Windows-Optimizer
+echo git clone https://github.com/Harvester57/Windows-PolicyRules
+echo #https://learn.microsoft.com/en-us/windows/security/operating-system-security/device-management/windows-security-configuration-framework/security-compliance-toolkit-10#what-is-the-policy-analyzer-tool
+echo #https://www.microsoft.com/en-us/download/details.aspx?id=55319
+echo git clone https://github.com/PusPC/Pus
+echo git clone https://github.com/itsNileshHere/devkit-lab
+echo git clone https://github.com/SysadminWorld/Bloatynosy
+echo git clone https://github.com/Scrut1ny/Windows-Debloating-Script
+echo git clone https://github.com/markkerry/Proactive-Remediations
+echo git clone https://github.com/azurejoga/Aurora-Windows-Optimizer
+echo git clone https://github.com/zoicware/RemoveWindowsAI
+#TOP
+echo git clone https://github.com/TheSPEEDO/URLRunner
+echo git clone https://github.com/arcadesdude/BRU
