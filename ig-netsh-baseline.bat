07466dca82e7e2d4b7dcd204fd0111def7974db9
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-01-04 21:00:38 -0600

    Delete ig-netsh-baseline.bat

--- ig-netsh-baseline.bat
+++
@@ -1,7 +0,0 @@
-echo https://ir.cnd.ca.gov/2.Identification/2.19-Create_firewall_rule_list/#manual-steps
-netsh advfirewall set allprofiles state on
-netsh advfirewall set allprofiles firewallpolicy blockinbound,allowoutbound
-netsh advfirewall set allprofiles logging filename %SystemRoot%\System32\LogFiles\Firewall\pfirewall.log
-netsh advfirewall set allprofiles logging maxfilesize 16384
-netsh advfirewall set allprofiles logging droppedconnections enable
-netsh advfirewall set allprofiles logging allowedconnections enable
