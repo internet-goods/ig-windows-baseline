483de94b82c75fd694635b12d1b9cdb35b73babd
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-01-23 06:13:24 -0600

    Create ig-backup.bat

---
+++ ig-backup.bat
@@ -0,0 +1,16 @@
+echo run all backup scripts to save current baseline
+echo DOS scripts first
+ig-auditpol-backup.bat
+ig-bcdedit-backup.bat
+ig-bitsadmin-backup.bat
+ig-certutil-backup.bat
+ig-copy-backup.bat
+ig-dism-backup.bat
+ig-net-accounts-backup.bat
+ig-netsh-backup.bat
+ig-reg-backup.bat
+ig-schtasks-backup.bat
+ig-startupfolder-backup.bat
+ig-systeminfo-backup.bat
+ig-w32tm-backup.bat
+ig-wmic-backup.bat
