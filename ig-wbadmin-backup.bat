37e933d94b4cbad8cf4ec198d4edc655bd9d47ec
internet-goods <4577617+internet-goods@users.noreply.github.com>
2026-02-02 18:58:36 -0600

    Update ig-wbadmin-backup.bat

--- ig-wbadmin-backup.bat
+++ ig-wbadmin-backup.bat
@@ -1,3 +1,6 @@
+echo WBAdmin (wbadmin.exe) is a powerful command-line utility in Windows (Vista/7 and Server 2008+) 
+echo used to back up and restore operating systems, volumes, files, folders, and applications. 
+echo It enables advanced data protection, including "bare-metal" backups, by leveraging Volume Shadow Copy Service (VSS) to create VHD/VHDX file
 echo backup wrapper, to backup C to D for example, run ig-wbadmin-backup.bat C: D:
 echo wbadmin start backup -backupTarget:D: -include:C: -allCritical -quiet
 set arg1=%2
